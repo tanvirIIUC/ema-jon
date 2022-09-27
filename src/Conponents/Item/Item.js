92a09d19f72f6ecd9f5d2f1512b2606e51afe66c
tanvirIIUC <tanvirsohaniiuc@gmail.com>
2022-09-27 21:05:26 +0600

    update

--- src/Conponents/Item/Item.js
+++ src/Conponents/Item/Item.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import './Item.css';
 
@@ -13,7 +15,10 @@ const Item = (props) => {
           <p><small>Manufacture : {seller}</small></p>
           <p><small>Rating : {ratings}</small></p>
           <div>
-          <button onClick={()=>props.handleAddToCArd(props.product)} className='button'>Add to Card</button>
+          <button  onClick={()=>props.handleAddToCArd(props.product)} className='button'>
+            <p>Add to Card</p>
+           <p><FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon></p> 
+            </button>
           </div>
           </div>
          
